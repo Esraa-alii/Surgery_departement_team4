e61e77b62fcffbdd6691f88595de162f9a232791
joeadham <yousefdhm@gmail.com>
2022-06-04 14:36:34 +0200

    Added the book appointment part to the backend project

--- backend/myapp/resources/views/dashboard_patient.blade.php
+++ backend/myapp/resources/views/dashboard_patient.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('/fontawesome-free-6.0.0-web/css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('styling/fontawesome-free-6.0.0-web/css/all.css') }}">
 
     <link rel="stylesheet" href="{{ asset('styling/bootstrap/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('styling/css/dashboard_patient.css') }}">
@@ -61,20 +61,22 @@
             <p> Room number : op123</p>
 
 
-            <button id="cancle" onclick="toggleText()">Cancel</button>
+            <button id="cancel" onclick="toggleText()">Cancel</button>
 
         </div>
 
-        <div id="no_op">
-            <h2>There's no comming operation</h2>
-            <a href="#" id="book">Book Appointment</a>
-        </div>
-        <div id="report">
-            <h2>After Operation details</h2>
-            <p> your preoperation medication will be on : Sun 2/6 </p>
-            <p> Please attend in the hospital at : 5:00 PM </p>
-            <button id="view" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">view report</button>
-        </div>
+  <div id="no_op">
+    <h2>There's no coming operation</h2>
+    <button id="book" type="button" data-bs-toggle="modal" data-bs-target="#exampleModalLabel_appo">Book Appointment</button>
+
+    <!-- <a href="#" id="book">Book Appointment</a> -->
+  </div>
+  <div id="report">
+    <h2>After Operation details</h2>
+    <p> your preoperation medication will be on : Sun 2/6 </p>
+    <p> Please attend in the hospital at : 5:00 PM </p>
+    <button id="view" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">view report</button>
+    </div>
 
     </div>
 
@@ -92,23 +94,163 @@
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
-
             </div>
-
         </div>
-
     </div>
 
-    @if (session()->has('success'))
-        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 4000)" x-show='show' class="alert alert-success">
-            <strong>{{ session('success') }}</strong>
-        </div>
-    @endif
-    <script src="{{ asset('/bootstrap/bootstrap.bundle.min.js') }}"></script>
+ <!-- modal popup for appointement -->
+ <div class="modal fade" id="exampleModalLabel_appo" tabindex="-1" aria-labelledby="exampleModalLabel_appo" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+     <div class="modal-content" >
+       <div class="modal-header">
+         <h3 class="modal-title" id="exampleModalLabel_appo" style="margin:0;">Book appointement </h3>
+       </div>
+       <div class="modal-body">
+        <form>
+          <h3 class="app_title"> Choose your Surgical Specialties</h3>
 
-    <script src="{{ asset('/Java Script/dashboard_patient.js') }}"></script>
+                <label>
+                  <input type="radio" name="nature">
+                  <img class="Specialties" id="cardio" onclick="showcardio()" src="{{asset("surgeryDepartement/cardio.jpeg")}}" alt="cardio">
+                  <h4>cardiothoracic</h4>
+                </label>
+
+                <label  >
+                  <input type="radio" name="nature" >
+                  <img class="Specialties" onclick="showurology()" src="{{asset("surgeryDepartement/urology.jpg")}}" alt="conference1">
+                  <h4>urology</h4>
+                </label>
 
 
+
+                <label>
+                  <input type="radio" name="nature" >
+                  <img class="Specialties" onclick="neuroshow()" src="{{asset("surgeryDepartement/neurology.jpg")}}" alt="conference1">
+                  <h4>neurological</h4>
+                </label>
+
+                <br>
+
+
+                <label>
+                  <input type="radio" name="nature" >
+                  <img class="Specialties" onclick="showOrthopaedic_doc()"src="{{asset("surgeryDepartement/orth2.jpeg")}}" id="orth" alt="conference1">
+                  <h4 id="or" >Orthopaedic</h4>
+                </label>
+                <label >
+                  <input type="radio" name="nature" >
+                  <img class="Specialties" onclick="dentalshow()" id="dental" src="{{asset("surgeryDepartement/dental.jpeg")}}" alt="conference1">
+                  <h4 id="dent">dental</h4>
+                </label>
+                <label >
+                  <input type="radio" name="nature" >
+                  <img class="Specialties" onclick="showObstetrics()" id="Obstetrics" src="{{asset("surgeryDepartement/Obstetrics.jpeg")}}" alt="conference1">
+                  <h4 id="obs">Obstetrics</h4>
+                </label>
+                  <!-- <button type="submit" id="Select"> Select </button>
+        </form> -->
+            <!-- Doctors -->
+            <br>
+            <hr>
+        <!-- <form > -->
+          <h3 class="app_title" id="do"> Choose your Doctor</h3>
+
+            <!-- cardio doc -->
+            <div id="cardio_doc">
+              <label >
+                <input type="radio" name="nature" >
+                <img class="doctors"  src="{{asset("surgeryDepartement/magdy_y.jpeg")}}" alt="conference1">
+                <h4 class="ma">Dr Magdy Yacoub</h4>
+              </label>
+              <label >
+                <input type="radio" name="nature" >
+                <img class="ahmed" src='{{asset("surgeryDepartement/ahmed helmy.jpeg")}}' alt="conference1">
+                <h4 class="a">Dr Ahmed helmy</h4>
+              </label>
+            </div>
+
+
+            <!-- urology doc -->
+            <div id="urology_doc">
+              <label >
+                <input type="radio" name="nature" >
+                <img class="abdel"  src="{{asset("surgeryDepartement/abdelnasser.jpeg")}}" alt="conference1">
+                <h4 class="abd">Dr Abd Elnassser</h4>
+              </label>
+            </div>
+
+
+             <!-- Obstetrics doc -->
+             <div id="Obstetrics_docs">
+              <label >
+              <input type="radio" name="nature" >
+              <img id="samya"  src="{{asset("surgeryDepartement/samya.jpeg")}}" alt="conference1">
+              <h4 id="sam">Dr Samya Mohamed</h4>
+            </label>
+
+            <label >
+              <input type="radio" name="nature" >
+              <img id="aida"  src="{{asset("surgeryDepartement/Dr.Aida_.jpg")}}" alt="conference1">
+              <h4 id="ad">Dr Aida </h4>
+            </label>
+            </div>
+
+            <!-- neurological doc -->
+            <div id="neurological_docs">
+              <label >
+              <input type="radio" name="nature" >
+              <img id="nabeel"  src="{{asset("surgeryDepartement/dr.-Mohammed-Nabeel-scaled.jpg")}}" alt="conference1">
+              <h4 id="n">Dr Mohammed Nabeel</h4>
+            </label>
+
+            <label >
+              <input type="radio" name="nature" >
+              <img id="wael"  src="{{asset("surgeryDepartement/wael mahmoud.jpeg")}}" alt="conference1">
+              <h4 id="w">Dr Wael Mahmoud</h4>
+            </label>
+            </div>
+
+            <!-- Orthopaedic doc -->
+            <div id="Orthopaedic_doc">
+              <label >
+              <input type="radio" name="nature" >
+              <img id="tayb"  src="{{asset("surgeryDepartement/ahmedeltayeb.png")}}" alt="conference1">
+              <h4 id="t">Dr Ahmed Eltaib</h4>
+            </label>
+
+            <label >
+              <input type="radio" name="nature" >
+              <img id="mostafa"  src="{{asset("surgeryDepartement/MostafaEzzat.png")}}" alt="conference1">
+              <h4 id="m">Dr Mostafa Ezzat</h4>
+            </label>
+            </div>
+
+
+            <!-- dental -->
+            <div id="dental_doc">
+              <label >
+              <input type="radio" name="nature" >
+              <img id="Eyman"  src="{{asset("surgeryDepartement/Eyman eltantawy.jpeg")}}" alt="conference1">
+              <h4 id="ey">Dr Eyman Eltantawy</h4>
+            </label>
+
+            <label >
+              <input type="radio" name="nature" >
+              <img id="gamal"  src="{{asset("surgeryDepartement/Khaled Gamal.png")}}" alt="conference1">
+              <h4 id="g">Dr Khaled Gamal</h4>
+            </label>
+            </div>
+        </form>
+       </div>
+       <div class="modal-footer">
+         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+       </div>
+     </div>
+   </div>
+ </div>
+    <script src="{{asset("styling/bootstrap/bootstrap.bundle.min.js")}}"></script>
+
+ <script src="{{asset("Java Script/dashboard_patient.js")}}"></script>
 </body>
 
 </html>
